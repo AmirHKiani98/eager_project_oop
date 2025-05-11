b5e6854e8bf5498f1deb358b2fdccbf2e3869673
Amirhossein Kiani <amirhkiani98@yahoo.com>
2025-05-11 18:57:01 -0500

    Add initial cache parameters for traffic simulation

--- src/model/ltm.py
+++ src/model/ltm.py
@@ -1,3 +1,35 @@
 """
 Implementation of the LTM traffic model
 """
+import logging
+import numpy as np
+from rich.logging import RichHandler
+from src.model.traffic_model import TrafficModel
+from src.common_utility.units import Units
+logging.basicConfig(
+    level="DEBUG",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)]
+)
+logger = logging.getLogger("rich")
+
+class LTM(TrafficModel):
+    """
+    Class representing a LTM traffic model.
+    """
+
+    def predict(self, **args):
+        """
+        Predicts the outcome of the LTM
+        """
+        pass
+
+    
+    def run(self, args):
+        """
+        Run the LTM model with the given arguments.
+        """
+        n_tlxw = args["n_tlxw"]
+        n_txvf = args["n_txvf"]
+        
