6f1b0fd43c1facfeead2d72fb2979b44a794d6b3
Amirhossein Kiani <amirhkiani98@yahoo.com>
2025-05-14 11:08:33 +0000

    Refactor test_point_queue to improve flow calculations and enhance clarity. Point queue works just fine

--- tests/model/test_pq.py
+++ tests/model/test_pq.py
@@ -24,7 +24,8 @@ def test_point_queue():
     dt = 1 * Units.HR
     q_max_up = 10 * Units.PER_HR
     q_max_down = 5 * Units.PER_HR
-    
+    L = 1 * Units.KM
+    uf = L/(3*dt)
     table = {
         "cumulative_count_upstream": [0, 1, 5, 10, 17, 27, 30, 30, 30, 30, 30],
         "cumulative_count_downstream": [0, 0, 0, 0, 1, 5, 10, 15, 20, 25, 30],
@@ -34,9 +35,16 @@ def test_point_queue():
         "sending_flow": [0, 0, 0, 1, 4, 5, 5, 5, 5, 5, 0],
         "receiving_flow": [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
     }
-    for i in range(len(table["cumulative_count_upstream"])):
-        cumulative_count_upstream = table["cumulative_count_upstream"][i]
-        cumulative_count_downstream = table["cumulative_count_downstream"][i]
+    for t in [i * Units.HR for i in range(len(expected_values["sending_flow"]))]:
+        t_before = int((t + dt - L/uf).to(Units.HR).value)
+        t_current = int(t.to(Units.HR).value)
+        if t_before < 0:
+            cumulative_count_upstream = 0
+            cumulative_count_downstream = 0
+        else:
+            cumulative_count_upstream = table["cumulative_count_upstream"][t_before]
+            cumulative_count_downstream = table["cumulative_count_downstream"][t_current]
+
         
         values = pq.run({
             "cumulative_count_upstream": cumulative_count_upstream,
@@ -46,6 +54,7 @@ def test_point_queue():
             "q_max_down": q_max_down,
             "next_occupancy": 0
         })
-        print(values, "\n", table["cumulative_count_upstream"][i], table["cumulative_count_downstream"][i])
+        i = int(t.to(Units.HR).value)
+
         assert values["sending_flow"] == expected_values["sending_flow"][i]
         assert values["receiving_flow"] == expected_values["receiving_flow"][i]
