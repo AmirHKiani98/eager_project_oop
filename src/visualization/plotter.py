e341accc3b22e8f9f2130ec3105eba5c6f11222e
Amirhossein Kiani <amirhkiani98@yahoo.com>
2025-05-18 15:50:51 -0500

    Add metadata and link files for new simulation data; remove obsolete heatmap images

--- src/visualization/plotter.py
+++ src/visualization/plotter.py
@@ -10,6 +10,7 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib
 import pandas as pd
+from tqdm import tqdm
 import numpy as np
 from shapely.geometry import Point as POINT
 from src.preprocessing.geo_loader import GeoLoader
@@ -38,6 +39,11 @@ class Plotter:
         self.link_lengths = {
             link_id: link.get_length().to(Units.M).value
             for link_id, link in self.geo_loader.links.items()
+        }
+        self.cell_length = {
+            link_id: {
+                cell_id: cell.get_length().to(Units.M).value for cell_id, cell in link.cells.items()
+            } for link_id, link in self.geo_loader.links.items()
         }
         
 
@@ -94,8 +100,9 @@ class Plotter:
             # pl.col('rmse') < 20
         )
         
-        folder_path = f"{self.cache_dir}/results/{traffic_model}/{self.get_base_name_without_extension(file_name)}"
-        os.makedirs(folder_path, exist_ok=True)
+        figure_path = f"{self.cache_dir}/results/{self.get_base_name_without_extension(file_name)}/{traffic_model}.png"
+        if not os.path.exists(os.path.dirname(figure_path)):
+            os.makedirs(os.path.dirname(figure_path))
         
         rmse_data_min = rmse_data["rmse"].min()
         rmse_data_max = rmse_data["rmse"].max()
@@ -117,7 +124,7 @@ class Plotter:
             vmin=rmse_data_min,
             vmax=rmse_data_max,
             annot=False,
-            cbar_kws={'label': 'RMSE'}
+            cbar_kws={'label': 'Error'}
         )
 
         plt.title("Flow Actual Heatmap")
@@ -126,10 +133,98 @@ class Plotter:
         plt.tight_layout()
 
         # Save the heatmap
-        plt.savefig(f"{folder_path}/heatmap.png")
+        plt.savefig(figure_path)
         plt.close()  
 
-    def plot_sns_heatmap_ctm(self, file_name: str):
+    def truncate_and_square_error_ctm(self, row):
+        new_occ = np.array(row["new_occupancy"])
+        next_occ = np.array(row["next_occupancy"])
+        lengths = np.array(row["cell_lengths"])
+
+        min_len = min(len(new_occ), len(next_occ), len(lengths))
+        return ((new_occ[:min_len] - next_occ[:min_len]) / lengths[:min_len]) ** 2
+
+    def plot_error_ctm(self,
+                       data_file_name: str,
+            hash_parmas: str,
+            hash_geo: str,
+            traffic_model: str):
+        """
+        Plots the error in CTM.
+        Args:
+            data_file_name (str): The name of the file to plot.
+            hash_parmas (str): The hash of the parameters.
+            hash_geo (str): The hash of the geo.
+            traffic_model (str): The name of the traffic model.
+        """
+        file_name = f"{self.cache_dir}/{traffic_model}/{data_file_name}_{hash_geo}_{hash_parmas}.json"
+        if not os.path.exists(file_name):
+            raise FileNotFoundError(f"File not found: {file_name}")
+        data = pl.read_json(
+            file_name
+        )
+        data = data.with_columns(
+            pl.col("link_id")
+            .cast(pl.Int64)
+            .map_elements(lambda link_id: list(self.cell_length.get(link_id, {}).values()))
+            .alias("cell_lengths")
+        )
+
+        data = data.with_columns(
+            pl.struct(["new_occupancy", "next_occupancy", "cell_lengths"])
+            .map_elements(self.truncate_and_square_error_ctm)
+            .alias("squared_error")
+        )
+        error_info = data.select(
+            ["link_id", "trajectory_time", "squared_error"]
+        )
+        groups = error_info.group_by(["link_id"])
+        figure_path = f"{self.cache_dir}/results/{self.get_base_name_without_extension(file_name)}/{traffic_model}/"
+        if not os.path.exists(figure_path):
+            os.makedirs(figure_path)
+        all_errors = error_info.to_pandas()["squared_error"].explode().astype(float)
+        rmse_data_min = all_errors.min()
+        rmse_data_max = all_errors.max()
+        for name, group in groups:
+            link_id = int(name[0]) # type: ignore
+            group = group.sort("trajectory_time")
+            rmse_data = {
+                "trajectory_time": [],
+                "squared_error": [],
+                "cell_id": [],
+            }
+            for row in tqdm(group.iter_rows(named=True), desc=f"Processing link {link_id}", total=len(group)):
+                trajectory_time = row["trajectory_time"]
+                squared_error = row["squared_error"]
+                for i in range(len(squared_error)):
+                    rmse_data["trajectory_time"].append(trajectory_time)
+                    rmse_data["squared_error"].append(squared_error[i])
+                    rmse_data["cell_id"].append(i)
+            rmse_data = pd.DataFrame(rmse_data)
+            heatmap_data = rmse_data.pivot(index="trajectory_time", columns="cell_id", values="squared_error")
+            plt.figure(figsize=(8, 6))
+            sns.heatmap(
+                heatmap_data,
+                cmap="Reds",
+                vmin=rmse_data_min,
+                vmax=rmse_data_max,
+                annot=False,
+                cbar_kws={'label': 'Error'}
+            )
+            plt.title(f"Heatmap for Link ID: {link_id}")
+            plt.savefig(figure_path + f"Link_{link_id}.png")
+            plt.close()
+        # data = data.with_columns(
+        #     pl.struct(["new_outflow", "next_occupancy", "cell_lengths"])
+        #     .map_elements(lambda row: ((np.array(row["new_outflow"]) - np.array(row["next_occupancy"])) / np.array(row["cell_lengths"]))**2)
+        #     .alias("squared_error")
+        # )
+        # print(data.select(["squared_error"]).head(10))
+
+
+    def plot_sns_heatmap_ctm(self, 
+                             file_name: str,
+                            traffic_model: str):
         """
         Plotting the heatmap using seaborn.
 
@@ -150,7 +245,7 @@ class Plotter:
             group = group.to_pandas().set_index("trajectory_time")
 
             plt.figure(figsize=(8, 6))
-            sns.heatmap(group, annot=True, fmt=".1f", cmap="RdYlGn_r", cbar=False)
+            sns.heatmap(group, annot=False, fmt=".1f", cmap="RdYlGn_r", cbar=True)
             plt.title(f"Heatmap for Link ID: {link_id}")
             plt.savefig(figure_path)
             plt.close()
@@ -207,6 +302,75 @@ class Plotter:
         plt.savefig(figure_path)
             
         
+    def plot_error_ltm(self,
+                       data_file_name,
+                       hash_parmas: str,
+                       hash_geo: str,
+                       traffic_model: str):
+        """
+        Plotting the error in LTM.
+        Args:
+            data_file_name (str): The name of the file to plot.
+            hash_parmas (str): The hash of the parameters.
+            hash_geo (str): The hash of the geo.
+            traffic_model (str): The name of the traffic model.
+        """
+        file_name = f"{self.cache_dir}/{traffic_model}/{data_file_name}_{hash_geo}_{hash_parmas}.json"
+        if not os.path.exists(file_name):
+            raise FileNotFoundError(f"File not found: {file_name}")
+        data = pl.read_json(
+            file_name
+        )
+        data = data.with_columns(
+            pl.struct(["link_id", "cell_id"])
+            .map_elements(lambda row: self.cell_length[int(row["link_id"])][int(row["cell_id"])], return_dtype=pl.Float64)
+            .alias("cell_length")
+        )
+
+        data = data.with_columns(
+            ((pl.col("new_occupancy") - pl.col("next_occupancy")) /
+            pl.col("cell_length")).pow(2).alias("squared_error")
+        )
+        data = data.with_columns(
+            pl.col("x").map_elements(lambda x: round(x, 2)
+                                     ).alias("x")                                     
+        )
+        error_info = data.select(
+            ["link_id", "trajectory_time", "squared_error", "x"]
+        )
+        groups = error_info.group_by(["link_id"])
+        figure_path = f"{self.cache_dir}/results/{self.get_base_name_without_extension(file_name)}/{traffic_model}/"
+        if not os.path.exists(figure_path):
+            os.makedirs(figure_path)
+        min_errors = error_info["squared_error"].cast(pl.Float64).min()
+        max_errors = error_info["squared_error"].max()
+        for name, group in groups:
+            link_id = int(name[0]) # type: ignore
+            group = group.sort("trajectory_time")
+            group = group.to_pandas()
+            rmse_data = group.pivot(
+                index="trajectory_time", columns="x", values="squared_error"
+            )
+            plt.figure(figsize=(8, 6))
+            sns.heatmap(
+                rmse_data,
+                cmap="Reds",
+                vmin=min_errors, # type: ignore
+                vmax=max_errors, # type: ignore
+                annot=False,
+                cbar_kws={'label': 'Error'}
+            )
+            plt.title(f"Heatmap for Link ID: {link_id}")
+            plt.xlabel("X")
+            plt.ylabel("Time")
+            plt.tight_layout()
+            plt.savefig(figure_path + f"Link_{link_id}.png")
+            plt.close()
+
+
+
+
+
 
 
 
@@ -222,11 +386,37 @@ class Plotter:
         """
         return os.path.splitext(os.path.basename(file_name))[0]
 
-    def plot(self):
+    def plot(self,
+             data_file_name: str,
+            hash_parmas: str,
+            hash_geo: str,
+            traffic_model: str):
         """
         Plotting the data
         """
-        pass
+        if traffic_model == "LTM":
+            self.plot_error_ltm(
+                data_file_name=data_file_name,
+                hash_parmas=hash_parmas,
+                hash_geo=hash_geo,
+                traffic_model=traffic_model
+            )
+        elif traffic_model == "CTM":
+            self.plot_error_ctm(
+                data_file_name=data_file_name,
+                hash_parmas=hash_parmas,
+                hash_geo=hash_geo,
+                traffic_model=traffic_model
+            )
+        elif traffic_model == "PointQueue" or traffic_model == "SpatialQueue":
+            self.plot_error_point_queue_spatial_queue(
+                data_file_name=data_file_name,
+                hash_parmas=hash_parmas,
+                hash_geo=hash_geo,
+                traffic_model=traffic_model
+            )
+        else:
+            raise ValueError(f"Traffic model {traffic_model} not supported")
     
     def animation(self, file_name: str):
         """
@@ -287,11 +477,11 @@ if __name__ == "__main__":
     data_file_name = "d1_20181029_0800_0830"
     params_hash = "dcca17e9025816395dbe6a5a465c2450"
     geo_hash = "682a48de"
-    traffic_model_name = "PointQueue"
+    traffic_model_name = "LTM"
     plotter = Plotter(cache_dir=".cache", geo_loader=model_geo_loader)
     # plotter.animation(f".cache/{data_file_name}_fully_process_vehicles_{geo_hash}.csv")
     # print("Heatmap generated and saved successfully.")
-    plotter.plot_error_point_queue_spatial_queue(
+    plotter.plot_error_ltm(
         data_file_name=data_file_name,
         hash_parmas=params_hash,
         hash_geo=geo_hash,
